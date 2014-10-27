c0d31566da7f2b90ea0eca3b26b46afaf3d9ee16
Matt Mawhinney <matt.mawhinney@gmail.com>
2014-10-27 11:47:54 -0400

    modifed d3Basics script.js

--- d3Basics/js/script.js
+++ d3Basics/js/script.js
@@ -23,19 +23,32 @@ var dataset = [ 5, 10, 15, 20, 25 ];
 //   .append("p")
 //   .text(function(d) { return d; });
 
-
 d3.select("body").selectAll("p")
   .data(dataset)
   .enter()
   .append("p")
-  .text(function(d) { return d; })
-  .style("color", function(d) {
-    if (d > 15) {   //Threshold of 15
-        return "red";
-    } else {
-        return "black";
-    }
-});
+  .text(function(d) {return "I can count up to " + d; });
+
+
+// d3.select("body").selectAll("p")
+//   .data(dataset)
+//   .enter()
+//   .append("p")
+//   .text("I can count up to " +  d);
+
+
+// d3.select("body").selectAll("p")
+//   .data(dataset)
+//   .enter()
+//   .append("p")
+//   .text(function(d) { return d; })
+//   .style("color", function(d) {
+//     if (d > 15) {   //Threshold of 15
+//         return "red";
+//     } else {
+//         return "black";
+//     }
+// });
 
 //console.log(d3.selectAll("p"))
 
