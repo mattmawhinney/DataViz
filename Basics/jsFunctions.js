6ed4af5444b566068a6adfbc10091eded9b8037e
Matt Mawhinney <matt.mawhinney@gmail.com>
2014-10-28 09:50:55 -0400

    added SVG stuff

--- Basics/jsFunctions.js
+++ Basics/jsFunctions.js
@@ -1,17 +1,67 @@
 //data types
-// strings, integers, booleans, arrays, objects
+// strings, numbers, booleans, arrays, objects
 //do some integer and string math
 
-5 *5 
-"Java" + "Script"
 
+
+
+//numbers
+5
+10
+5 + 10
+5 * 10
+
+5.5 * 5.5
+5.5 * 3 
+
+//strings 
+"Java"
+"Script"
+"Java" + "Script"
 "HTML" + 5
 
+//booleans
+true
+false
+
+5 < 10
+5 > 10 
+
+//booleans are extemely important for "control flow"
+// if something is true do the X, otherwise do Y (which is oftentimes the opposite)
+
+
+//variables
 //varibalize your name and log it
 var name = "Matt";
 
 console.log(name)
 console.log("name");
+
+
+//arrays
+//varibalize array 
+var myArray = [1,2,3,4,5,6]
+myArray[0]
+
+
+//objects
+
+var myObj = {
+  name: "Matt",
+  age: 30,
+  favoriteFoods: ["pizza", "pasta", "paella"]
+}
+
+myObj.favoriteFoods
+
+var myJSONObj = {
+  "name": "Matt",
+  "age": 30,
+  "favoriteFoods": ["pizza", "pasta", "paella"]
+}
+
+myObj.favoriteFoods
 
 //write named function to log name
 function sayHello(name) {
